3d1611c6596ecd1d35fd76ede02728a48708850c
iebb <1@wa.vg>
2019-08-15 09:00:58 +0200

    commitv2 :: Thu Aug 15 09:00:57 CEST 2019

--- db/common/boot_asset.db_.sql
+++ db/common/boot_asset.db_.sql
@@ -185,6 +185,11 @@ INSERT INTO "boot_asset_m" VALUES(174,'assets/image/login/startup_mu_1.png','ass
 INSERT INTO "boot_asset_m" VALUES(175,'assets/image/login/startup_mu_1.png','assets/sound/login/startup_mu_1.mp3','2019/08/06 00:00:00','2019/08/15 14:59:59',1,2,NULL,NULL);
 INSERT INTO "boot_asset_m" VALUES(176,'assets/image/login/startup_aq_4.png','assets/sound/login/startup_aq_4.mp3','2019/08/05 15:00:00','2019/08/20 23:59:59',2,18,NULL,NULL);
 INSERT INTO "boot_asset_m" VALUES(177,'assets/image/login/startup_aq_3.png','assets/sound/login/startup_aq_3.mp3','2019/08/05 15:00:00','2019/08/15 14:59:59',2,6,NULL,NULL);
+INSERT INTO "boot_asset_m" VALUES(178,'assets/image/login/startup_mu_1.png','assets/sound/login/startup_mu_1.mp3','2019/08/15 15:00:00','2019/08/20 23:59:59',1,2,NULL,NULL);
+INSERT INTO "boot_asset_m" VALUES(179,'assets/image/login/startup_mu_1.png','assets/sound/login/startup_mu_1.mp3','2019/08/21 00:00:00','2020/06/30 23:59:59',1,1,NULL,NULL);
+INSERT INTO "boot_asset_m" VALUES(180,'assets/image/login/startup_aq_3.png','assets/sound/login/startup_aq_3.mp3','2019/08/15 15:00:00','2019/08/20 23:59:59',2,6,NULL,NULL);
+INSERT INTO "boot_asset_m" VALUES(181,'assets/image/login/startup_aq_3.png','assets/sound/login/startup_aq_3.mp3','2019/08/21 00:00:00','2019/08/31 14:59:59',2,4,NULL,NULL);
+INSERT INTO "boot_asset_m" VALUES(182,'assets/image/login/startup_aq_3.png','assets/sound/login/startup_aq_3.mp3','2019/08/31 15:00:00','2020/06/30 23:59:59',2,2,NULL,NULL);
 CREATE TABLE `boot_bushimo_voice_m` (
     `boot_bushimo_voice_id` INTEGER NOT NULL,
     `voice_asset` TEXT NOT NULL,
