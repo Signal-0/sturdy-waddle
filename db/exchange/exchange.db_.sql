6bf6e1f8ca1c17395a4d912845acf63082b31f15
iebb <1@wa.vg>
2020-09-15 09:01:10 +0200

    commitv2 :: Tue 15 Sep 2020 09:01:10 AM CEST

--- db/exchange/exchange.db_.sql
+++ db/exchange/exchange.db_.sql
@@ -556,6 +556,7 @@ INSERT INTO "exchange_nopoint_unit_m" VALUES(2484);
 INSERT INTO "exchange_nopoint_unit_m" VALUES(2499);
 INSERT INTO "exchange_nopoint_unit_m" VALUES(2505);
 INSERT INTO "exchange_nopoint_unit_m" VALUES(2507);
+INSERT INTO "exchange_nopoint_unit_m" VALUES(2512);
 CREATE TABLE `exchange_point_m` (
     `exchange_point_id` INTEGER NOT NULL,
     `name` TEXT NOT NULL,
