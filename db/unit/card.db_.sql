b06aff4ae5fa8b36f3501c0ee6e3781d51cf8bc5
iebb <1@wa.vg>
2020-10-20 17:00:39 +0200

    commitv2 :: Tue 20 Oct 2020 05:00:39 PM CEST

--- db/unit/card.db_.sql
+++ db/unit/card.db_.sql
@@ -4595,7 +4595,7 @@ INSERT INTO "card_m" VALUES(7284,0,4411,0,0,-180,-142,0.9,'assets/image/unit/411
 INSERT INTO "card_m" VALUES(7285,0,4412,0,0,-180,-142,0.9,'assets/image/unit/41105006/b_rankup_41105006.png',NULL,308);
 INSERT INTO "card_m" VALUES(7286,0,4413,0,0,-180,-142,0.9,'assets/image/unit/43107007/b_normal_43107007.png',NULL,308);
 INSERT INTO "card_m" VALUES(7287,0,4414,0,0,-180,-142,0.9,'assets/image/unit/43107007/b_rankup_43107007.png',NULL,308);
-INSERT INTO "card_m" VALUES(7288,0,4415,0,0,-180,-142,0.9,'','XtM6NMdrXckIc58Fqvpnb2JGsM21w3gG0jvH7v66BiS399xZx/LapOMrXjr0bYX7nGZXbv+LBIFwqMeC6U+1Pv1EqBZUMlrgp7GNpCdNwLk1HzW8TVxsgY4X5BM9QxAH',1417);
+INSERT INTO "card_m" VALUES(7288,0,4415,0,0,-180,-142,0.9,'assets/image/unit/41002008/b_rankup_41002008.png',NULL,1417);
 INSERT INTO "card_m" VALUES(7289,0,4416,0,0,-180,-142,0.9,'','quxxnShfEkjzGexbeZsPbdx8GXJ6anMFjIYaULJw6hgd5p47GjeiVJ5QXbLfTdGHgt6JrBVGaIgxJvLQLoND8hUEtKR2Wv0ufmkmkNVGYBGjvFZ7aUumHecfRCH7XiIO',1421);
 INSERT INTO "card_m" VALUES(7290,0,2058,0,0,-180,-142,0.9,'assets/image/units/b_rankup_43004008_signed.png',NULL,308);
 INSERT INTO "card_m" VALUES(7291,0,2147,0,0,-180,-142,0.9,'assets/image/units/b_rankup_42008007_signed.png',NULL,308);
